ef05c44ada038c06acb238630075b8c067601e77
Scott <mogulsd@muohio.edu>
2012-10-28 21:18:23 -0400

    trying to get rid of duplicates
    working on getting rid duplicate entries in the array

--- main.cpp
+++ main.cpp
@@ -10,12 +10,12 @@ using namespace std;
 
 int main(){
 	ifstream myfile;
-	string words;
 	Entry* starbucksLocations;
 	starbucksLocations = new Entry[7700];
 	myfile.open("Starbucks_2006.csv");
-	words="";
 	int i = 0;
+	bool found = false;
+	string identifier = "";
 	if(myfile.fail())
 	{
 		cout << "Error: main() ; Failed to open file: " << endl;
@@ -24,18 +24,29 @@ int main(){
 	else
     {
 		while(!myfile.eof()){
-			getline(myfile, starbucksLocations[i].identifier,',');
-			myfile>>starbucksLocations[i].x;
-			myfile.get();
-			myfile>>starbucksLocations[i].y;
-			myfile.get();
-			i++;
+			//tried to eliminate duplicates
+			/*getline(myfile, starbucksLocations[i].identifier,',');
+			identifier = starbucksLocations[i].identifier;
+
+			for(int j=0;j<i;j++){
+				if(starbucksLocations[j].identifier==identifier){
+					found = true;
+				}
+			}
+			if(!found){*/
+				getline(myfile, starbucksLocations[i].identifier,',');
+				myfile>>starbucksLocations[i].x;
+				myfile.get();
+				myfile>>starbucksLocations[i].y;
+				myfile.get();
+				i++;
+			//}
 		}
 		myfile.close();
 		mogulsdStarbucks* starbucks = new mogulsdStarbucks();
 		starbucks ->build(starbucksLocations,7655);
 		Entry* nearest = starbucks ->getNearest(.5,.5);
-		cout<<"Nearest location"<<nearest->identifier<<" "<<nearest->x<<" "<<nearest->y<<endl;
+		cout<<"Nearest location "<<nearest->identifier<<" "<<nearest->x<<" "<<nearest->y<<endl;
 	}
 
 
